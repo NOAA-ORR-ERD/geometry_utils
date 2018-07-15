4743c2790bcc3f090da05ea33e0cdee05678a575
Chris Barker <Chris.Barker@noaa.gov>
2018-07-15 14:44:41 -0500

    setup.py set up to build cython extension

--- setup.py
+++ setup.py
@@ -1,19 +1,33 @@
 from setuptools import setup
 import versioneer
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+
+import numpy  # for the includes for the Cython code
 
 # package requirements go here
 requirements = ["numpy",
                 ]
 
+ext_modules = [Extension("geometry_utils.cy_point_in_polygon",
+                         sources=["geometry_utils/cy_point_in_polygon.pyx",
+                                  "geometry_utils/c_point_in_polygon.c"],
+                         include_dirs=[numpy.get_include()]),
+               ]
+
+cmdclass = versioneer.get_cmdclass()
+cmdclass['build_ext'] = build_ext
+
 setup(
     name='geometry_utils',
     version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
+    cmdclass=cmdclass,
     description="Utilities for basic computational geometry directly with numpy arrays.",
     author="Christopher Barker, Rob Hetland",
     author_email='Chris.Barker@noaa.gov',
     url='https://github.com/NOAA-ORR-ERD/geometry_utils',
     packages=['geometry_utils'],
+    ext_modules=ext_modules,
     install_requires=requirements,
     keywords='geometry_utils',
     classifiers=[
